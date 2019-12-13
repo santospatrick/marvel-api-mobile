0907ffe057a94433ac0057b68f9021779cda7d96
Patrick Santos <patrickspteixeira@gmail.com>
2019-12-13 20:51:45 -0300

    adiciona chamada para api da marvel

--- App.js
+++ App.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text } from 'react-native';
+import api from './src/services/api';
 
 const App = () => {
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    async function loadCharacters() {
+      const response = await api.get('/v1/public/comics');
+      setList(response.data.data.list);
+    }
+
+    // loadCharacters();
+  }, []);
+
   return (
     <View
       style={{
