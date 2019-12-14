3cad86979d106caa36aa36e95605584ad686eb0b
Patrick Santos <patrickspteixeira@gmail.com>
2019-12-14 13:32:21 -0300

    adiciona get de cada heroi

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
-import { View, ImageBackground } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, ImageBackground, ActivityIndicator } from 'react-native';
 import char from './3dman.json';
 import { FlatList } from 'react-native-gesture-handler';
 import IconButton from '../components/IconButton.js';
+import api from '../services/api.js';
 
 const actions = [
   { icon: 'contacts', label: 'Perfil' },
@@ -12,6 +13,27 @@ const actions = [
 
 const Detail = ({ navigation }) => {
   const [character, setCharacter] = useState(char);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const id = navigation.getParam('id');
+    async function loadCharacter() {
+      const response = await api.get(`/v1/public/characters/${id}`);
+      const [char] = response.data.data.results;
+      setCharacter(char);
+      setLoading(false);
+    }
+
+    // loadCharacter();
+  }, []);
+
+  if (loading) {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <ActivityIndicator color="red" />
+      </View>
+    );
+  }
 
   return (
     <View style={{ flex: 1 }}>
@@ -19,9 +41,7 @@ const Detail = ({ navigation }) => {
         source={{
           uri: `${character.thumbnail.path}.${character.thumbnail.extension}`,
         }}
-        style={{
-          flex: 1,
-        }}
+        style={{ flex: 1 }}
       >
         <FlatList
           data={character.urls}
@@ -29,7 +49,7 @@ const Detail = ({ navigation }) => {
           contentContainerStyle={{
             flex: 1,
             padding: 5,
-            paddingBottom: 30,
+            paddingBottom: 20,
             justifyContent: 'flex-end',
           }}
           keyExtractor={item => item.url}
